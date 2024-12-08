2eae3f289d228b784061512965bb6e51add2f788
priyanshu000920195 <priyanshukanabhai.kuchhadiya@edu.sait.ca>
2024-12-08 15:26:21 -0700

    "Update 'Don't have an account?' text in LoginPage"

--- app/login/page.js
+++ app/login/page.js
@@ -108,7 +108,7 @@ export default function LoginPage() {
         </div>
         <div className="mt-4 text-center">
           <p className="text-sm">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="text-purple-600 hover:underline">
               Sign Up
             </Link>
